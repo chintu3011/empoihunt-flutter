d80a70ade63f5fbbc6c2cf570ab869829764da82
ParthAmriSystems <parth.amrisystems@gmail.com>
2024-01-12 12:34:08 +0530

    update main.dart use try catch for fcm token

--- lib/main.dart
+++ lib/main.dart
@@ -14,54 +14,52 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'firebase_options.dart';
 import 'frame_work/repository/services/hive_service/hive_adapter.dart';
 
-Future<void> main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+    options: DefaultFirebaseOptions.currentPlatform,);
   await FirebaseMessaging.instance.setAutoInitEnabled(true);
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+  SystemChrome.setPreferredOrientations(
+      [ DeviceOrientation.portraitUp, DeviceOrientation.portraitDown,]);
+  await
+  SharedPrefServices.services
+      .init();
   String? fcmToken;
-  while(fcmToken ==null || fcmToken ==""){
+  try {
     fcmToken = await FirebaseMessaging.instance.getToken();
+  } catch (e) {
+    print(e.toString());
   }
-  ///--------- initialize Share preference --------///
-  await SharedPrefServices.services.init();
-  await SharedPrefServices.services.pref.setString(fcmTokenKey,fcmToken);
-  print("FCM Token ${SharedPrefServices.services.getString(fcmTokenKey)}");
+
+  ///--------- initialize Share preference --------/// await
+  SharedPrefServices.services
+      .pref.setString(fcmTokenKey, fcmToken ?? "");
+  print("FCM Token ${
+      SharedPrefServices.services
+          .getString(fcmTokenKey)}");
 
   /// ------- Hive open Box Service ---------///
-  registerHiveAdapters();
-  await Hive.initFlutter();
-  BoxService.boxService.nativeDeviceBox = await Hive.openBox<NativeDeviceDetailModel>(nativeDeviceDetailsBox);
-  BoxService.boxService.userGetDetailBox = await Hive.openBox<UserDetailDataModel>(userDetailsBox);
-  BoxService.boxService.userModelBox = await Hive.openBox<UserModel>(userModelBox);
-  BoxService.boxService.userExperienceBox = await Hive.openBox<UserExperienceModel>(userExperienceBox);
-  runApp(const ProviderScope(child: MyApp()));
-
+   registerHiveAdapters();
+   await Hive.initFlutter();
+   BoxService.boxService.nativeDeviceBox = await Hive.openBox<NativeDeviceDetailModel>(nativeDeviceDetailsBox);
+   BoxService.boxService.userGetDetailBox = await Hive.openBox<UserDetailDataModel>(userDetailsBox);
+   BoxService.boxService.userModelBox = await Hive.openBox<UserModel>(userModelBox);
+   BoxService.boxService.userExperienceBox = await Hive.openBox<UserExperienceModel>(userExperienceBox);
+   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: AppColors.colors.clayColors,
-    ));
-    return  ScreenUtilInit(
-      designSize: const Size(375, 812),
+  @override Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: AppColors.colors.clayColors,));
+    return ScreenUtilInit(designSize: const Size(375, 812),
       minTextAdapt: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
+      child: MaterialApp(debugShowCheckedModeBanner: false,
         theme: ThemeData(useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.colors.blueColors),
-        ),
-        home: const Splash(),
-      ),
-    );
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: AppColors.colors.blueColors),),
+        home: const Splash(),),);
   }
 }
